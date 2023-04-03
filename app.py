ab152596a1c4b23e8483948254197be302f3756e
sallymohamud999 <128308670+sallymohamud999@users.noreply.github.com>
2023-04-03 09:37:31 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,43 @@
-pip install pandas 
+from flask import Flask, render_template, request
+import joblib
+import numpy as np
+import pandas as pd
+
+model_svm = joblib.load('model_predict/svm.pkl')
+#model_lgbm = joblib.load('model_predict/lgbm.pkl')
+model_catboost = joblib.load('model_predict/catboost.pkl')
+tfidf = joblib.load('model_predict/tfidf.pkl')
+word_tokenize = joblib.load('model_predict/underthesea.pkl')
+
+app = Flask(__name__)
+
+@app.route('/')
+def man():
+    return render_template('home.html') 
+
+@app.route('/predict', methods=['POST'])
+
+def home():
+    text = request.form['input']
+    request_model = request.form['model']
+
+    X = word_tokenize(text, format="text")
+    features = tfidf.transform([X]).toarray()
+    
+    if request_model == "2":
+        model = model_catboost
+    elif request_model == "3":
+        model = model_catboost
+    else:
+        model = model_svm
+        
+    print(X)
+    print(request_model)  
+    print(model)
+    
+    pred = model.predict_proba(features) [:,1]
+
+    return render_template('after.html', proba = pred)
+
+if __name__ == "__main__":
+    app.run(debug=False)
